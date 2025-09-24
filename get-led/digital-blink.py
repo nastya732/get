fe2c7c15df972bb67b0fcb4f30d1f4a4eebbd4c1
nastya732 <rebic.anastasia@gmail.com>
2025-09-24 18:54:19 +0300

    New 4

--- get-led/digital-blink.py
+++ get-led/digital-blink.py
@@ -3,9 +3,9 @@ import time
 GPIO.setmode(GPIO.BCM)
 led = 26
 GPIO.setup(led, GPIO.OUT)
-state = 0
+state = 0   
 period = 1.0
 while True:
     GPIO.output(led, state)
     state = not state 
-    time.sleep(0.6)
+    time.sleep(period)
