f128d34aaf86917d6c2ed740ddccba810686af4d
nastya732 <rebic.anastasia@gmail.com>
2025-10-29 17:26:07 +0300

    Beter algoritm

--- get-adc/r2r_adc.py
+++ get-adc/r2r_adc.py
@@ -1,21 +1,6 @@
+# r2r_adc.py
 import RPi.GPIO as GPIO
 import time
-
-# # Временный скрипт для измерения
-# import RPi.GPIO as GPIO
-# import time
-
-# # Настройка GPIO как в вашем классе
-# bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(bits_gpio, GPIO.OUT)
-
-# # Подаем все единицы (255 для 8 бит) - максимальное напряжение
-# binary_255 = [1,1,1,1,1,1,1,1]
-# for i, pin in enumerate(bits_gpio):
-#     GPIO.output(pin, binary_255[i])
-
-# print("Подано 255 на ЦАП. Измерьте напряжение мультиметром!")
 
 class R2R_ADC:
     def __init__(self, dynamic_range, compare_time=0.01, verbose=False):
@@ -55,38 +40,29 @@ class R2R_ADC:
     
     def sequential_counting_adc(self):
         """Последовательный подсчет АЦП"""
-        max_value = 2**len(self.bits_gpio) - 1  # 255 для 8 бит
+        max_value = 2**len(self.bits_gpio) - 1
         
         for number in range(max_value + 1):
-            # Подаем число на ЦАП
             self.number_to_dac(number)
-            
-            # Пауза для стабилизации компаратора
             time.sleep(self.compare_time)
-            
-            # Читаем состояние компаратора
             comparator_output = GPIO.input(self.comp_gpio)
             
             if self.verbose:
                 print(f"Число: {number}, Компаратор: {comparator_output}")
             
-            # Если напряжение ЦАП превысило входное напряжение (компаратор = 1)
             if comparator_output == 1:
                 if self.verbose:
                     print(f"Напряжение превышено при числе: {number}")
                 return number
         
-        # Если не превысили - возвращаем максимальное значение
         if self.verbose:
             print(f"Достигнут максимум: {max_value}")
         return max_value
     
     def get_sc_voltage(self):
-        """Возвращает измеренное напряжение в Вольтах"""
+        """Возвращает измеренное напряжение в Вольтах (последовательный подсчет)"""
         digital_value = self.sequential_counting_adc()
-        max_digital = 2**len(self.bits_gpio) - 1  # 255 для 8 бит
-        
-        # Преобразуем цифровое значение в напряжение
+        max_digital = 2**len(self.bits_gpio) - 1
         voltage = (digital_value / max_digital) * self.dynamic_range
         
         if self.verbose:
@@ -94,42 +70,76 @@ class R2R_ADC:
         
         return voltage
 
+    def successive_approximation_adc(self):
+        """Алгоритм бинарного поиска напряжения на входе АЦП"""
+        max_value = 2**len(self.bits_gpio) - 1
+        result = 0
+        
+        # Проходим по всем битам, начиная со старшего
+        for bit in range(len(self.bits_gpio) - 1, -1, -1):
+            # Устанавливаем текущий бит в 1
+            current_value = result | (1 << bit)
+            
+            # Подаем значение на ЦАП
+            self.number_to_dac(current_value)
+            time.sleep(self.compare_time)
+            
+            # Читаем компаратор
+            comparator_output = GPIO.input(self.comp_gpio)
+            
+            if self.verbose:
+                print(f"Бит {bit}: значение {current_value}, компаратор: {comparator_output}")
+            
+            # Если напряжение ЦАП <= входного напряжения (компаратор = 0)
+            # Оставляем бит установленным
+            if comparator_output == 0:
+                result = current_value
+            # Иначе (компаратор = 1) - напряжение ЦАП > входного напряжения
+            # Сбрасываем текущий бит
+            else:
+                result = result & ~(1 << bit)
+        
+        if self.verbose:
+            print(f"Найдено значение: {result}")
+        
+        return result
+    
+    def get_sar_voltage(self):
+        """Возвращает измеренное алгоритмом бинарного поиска напряжение в Вольтах"""
+        digital_value = self.successive_approximation_adc()
+        max_digital = 2**len(self.bits_gpio) - 1
+        voltage = (digital_value / max_digital) * self.dynamic_range
+        
+        if self.verbose:
+            print(f"SAR - Цифровое значение: {digital_value}, Напряжение: {voltage:.3f} В")
+        
+        return voltage
 
-# # Основной охранник
-# if __name__ == "__main__":
-#     try:
-#         # Создаем объект АЦП (динамический диапазон нужно измерить мультиметром!)
-#         # Например, для 1.357В питания: dynamic_range=1.357
-#         adc = R2R_ADC(dynamic_range=1.357, verbose=True)
-        
-#         # Бесконечный цикл измерений
-#         while True:
-#             # Читаем напряжение
-#             voltage = adc.get_sc_voltage()
-            
-#             # Печатаем в терминал
-#             print(f"Измеренное напряжение: {voltage:.3f} В")
-            
-#             # Пауза между измерениями
-#             time.sleep(1)
-#     finally:
-#         # Вызываем деструктор
-#         if 'adc' in locals():
-#             adc.__del__()
-#         print("Программа завершена, GPIO очищен")
+
+# Основной охранник для SAR АЦП
 if __name__ == "__main__":
-    adc = None
     try:
-        # Создаем АЦП
-        adc = R2R_ADC(dynamic_range=1.357, verbose=True)
+        # Создаем объект АЦП (dynamic_range замените на измеренное значение!)
+        adc = R2R_ADC(dynamic_range=3.3, verbose=True)
         
-        # ОДНО измерение - достаточно для проверки работы
-        voltage = adc.get_sc_voltage()
-        print(f="Финальное измеренное напряжение: {voltage:.3f} В")
-        print("АЦП успешно отработал! Программа завершена.")
+        print("SAR АЦП запущен! Крутите потенциометр и наблюдайте изменение напряжения.")
+        print("Нажмите Ctrl+C для остановки.")
+        print("-" * 50)
         
-    except Exception as e:
-        print(f"Ошибка: {e}")
+        # Бесконечный цикл измерений
+        while True:
+            # Читаем напряжение методом get_sar_voltage()
+            voltage = adc.get_sar_voltage()
+            
+            # Печатаем в терминал
+            print(f"SAR Напряжение: {voltage:.3f} В", end='\r')
+            
+            time.sleep(0.1)
+            
+    except KeyboardInterrupt:
+        print("\n\nИзмерения остановлены пользователем")
     finally:
-        if adc:
+        # Вызываем деструктор
+        if 'adc' in locals():
             adc.__del__()
+        print("GPIO очищен, программа завершена.")
